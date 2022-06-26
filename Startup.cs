7be9aa5ecd70230bf944247a630946c06d76eaef
Jefry Castellón <jefry.castellon2@outlook.com>
2022-06-26 17:15:31 -0600

    Agregar archivos de proyecto.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PruebaServicios.Startup))]
+namespace PruebaServicios
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
